2326d0256172a99d057082c1ee3cac243ef681cc
kkulek <106318909+kkulek@users.noreply.github.com>
2023-03-12 21:39:22 +0100

    added partytown and umami analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
+import partytown from "@astrojs/partytown";
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://komornikkarolak.pl',
-  integrations: [react(), tailwind(), sitemap(), robotsTxt()]
+	site: "https://komornikkarolak.pl",
+	integrations: [
+		react(),
+		tailwind(),
+		sitemap(),
+		robotsTxt(),
+		partytown({
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		}),
+	],
 });
